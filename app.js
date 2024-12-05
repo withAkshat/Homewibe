d368743278f8d0f1e424283848442eb79495bc09
Akshat Bamania <akshat.bamania23@gmail.com>
2024-12-05 20:08:05 +0530

    Forth commit

--- app.js
+++ app.js
@@ -1,73 +1,89 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, (req,res)=>{
+app.listen(3000, (req, res) => {
     console.log("server is listening to port 3000");
-    
-   
+
+
 })
 
 const path = require('path');
-app.set("view engine", "ejs" )
-app.set("views", path.join(__dirname,"views"))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose');
 
-async function main(){
+async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
 main()
-.then((res)=>{
-    console.log("Connection is Working");
-})
-.catch((err)=>{
-    console.log(err);    
-})
+    .then((res) => {
+        console.log("Connection is Working");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 const Listing = require("./models/listing.js");
 const { log } = require('console');
+const { monitorEventLoopDelay } = require('perf_hooks');
 
 // ----------------------------------------------------------------------------------
 
-app.get("/listings",async (req,res)=>{
+app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({})
     // .then((res)=>{
     //     console.log(res);
     // })
     // .catch((err)=>{
     //     console.log("Error Occured:",err);
-        
+
     // })
 
-    res.render("listings/index.ejs" ,{ allListings })
+    res.render("listings/index.ejs", { allListings })
 });
-
-
-// Read or Show Route
-
-app.get("/listings/:id", async (req,res)=>{
-    let { id } = req.params;
-    let  indData = await Listing.findById(id);
-    
-    
-    // res.send("working");
-    res.render("listings/show.ejs" , { indData } )
-})
 
 
 // New Route
 
-app.get("/listings/new", async (req,res)=>{
-    
+app.get("/listings/new", (req, res) => {
+
     res.render("listings/new.ejs")
 })
 
 
-// app.get("/testListing",async (req,res)=>{
+// Create Listing
+
+app.post("/listings", (req, res) => {
+    let listing = req.body.listing;
+
+    let newListing = new Listing(listing)
+
+    console.log(newListing);
     
+
+
+})
+
+
+// Read or Show Route
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let indData = await Listing.findById(id);
+
+
+    // res.send("working");
+    res.render("listings/show.ejs", { indData })
+})
+
+
+
+// app.get("/testListing",async (req,res)=>{
+
 //     let sampleListing = new Listing({
 //   
 
@@ -75,9 +91,9 @@ app.get("/listings/new", async (req,res)=>{
 // -------------------------------------------------------------------------------------
 
 
-app.get("/", (req,res)=>{
-    
+app.get("/", (req, res) => {
+
     res.send("Root Directory")
-    
+
 })
 
